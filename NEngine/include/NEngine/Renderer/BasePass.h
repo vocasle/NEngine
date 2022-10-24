4dc151b3d5460519eef7adf286fbce928ac805e1
Nikita Elsakov <vocasle@outlook.com>
2022-10-24 15:59:45 +0300

    Add const buffers to BasePass

--- NEngine/include/NEngine/Renderer/BasePass.h
+++ NEngine/include/NEngine/Renderer/BasePass.h
@@ -7,6 +7,7 @@
 #include "NEngine/Helpers/DeviceResources.h"
 #include "PixelShader.h"
 #include "VertexShader.h"
+#include "NEngine/Helpers/DynamicConstBuffer.h"
 
 namespace NEngine::Renderer {
 class BasePass
@@ -22,5 +23,8 @@ protected:
     std::unique_ptr<VertexShader> mVertexShader;
     std::unique_ptr<PixelShader> mPixelShader;
     std::unique_ptr<InputLayout> mInputLayout;
+    std::unique_ptr<Helpers::DynamicConstBuffer> mPerFrameBuffer;
+    std::unique_ptr<Helpers::DynamicConstBuffer> mPerSceneBuffer;
+    std::unique_ptr<Helpers::DynamicConstBuffer> mPerObjectBuffer;
 };
 }  // namespace NEngine::Renderer
--- NEngine/src/Renderer/BasePass.cpp
+++ NEngine/src/Renderer/BasePass.cpp
@@ -3,6 +3,7 @@
 #include "NEngine/Utils/Utils.h"
 
 using namespace NEngine::Utils;
+using namespace NEngine::Helpers;
 
 void
 NEngine::Renderer::BasePass::Draw(Helpers::DeviceResources &deviceResources,
@@ -31,4 +32,59 @@ NEngine::Renderer::BasePass::BasePass(Helpers::DeviceResources &deviceResources)
     const auto pixelPath =
         UtilsFormatStr("%s/%s", NENGINE_SHADER_BUILD_DIR, "PhongPS.cso");
     mPixelShader = std::make_unique<PixelShader>(deviceResources, pixelPath);
+
+    {
+        DynamicConstBufferDesc desc;
+        desc.AddNode(Node("worldInvTranspose", NodeType::Float4X4));
+        desc.AddNode(Node("world", NodeType::Float4X4));
+        desc.AddNode(Node("shadowTransform", NodeType::Float4X4));
+        Node material("material", NodeType::Struct);
+        material.AddChild("roughness", NodeType::Float);
+        desc.AddNode(material);
+
+        mPerObjectBuffer = std::make_unique<DynamicConstBuffer>(desc, deviceResources);
+    }
+
+    {
+        DynamicConstBufferDesc desc;
+        desc.AddNode(Node("view", NodeType::Float4X4));
+        desc.AddNode(Node("proj", NodeType::Float4X4));
+        desc.AddNode(Node("cameraPosW", NodeType::Float3));
+
+        mPerFrameBuffer = std::make_unique<DynamicConstBuffer>(desc, deviceResources);
+    }
+
+    {
+        DynamicConstBufferDesc desc;
+        Node dirLight("dirLight", NodeType::Struct);
+        dirLight.AddChild("Ambient", NodeType::Float);
+        dirLight.AddChild("Diffuse", NodeType::Float);
+        dirLight.AddChild("Specular", NodeType::Float);
+        dirLight.AddChild("Position", NodeType::Float);
+
+        Node spotLight("spotLight", NodeType::Struct);
+        spotLight.AddChild("Ambient", NodeType::Float);
+        spotLight.AddChild("Diffuse", NodeType::Float);
+        spotLight.AddChild("Specular", NodeType::Float);
+        spotLight.AddChild("Position", NodeType::Float);
+        spotLight.AddChild("Direction", NodeType::Float);
+
+        Node pointLight("pointLight", NodeType::Struct);
+        pointLight.AddChild("Ambient", NodeType::Float);
+        pointLight.AddChild("Diffuse", NodeType::Float);
+        pointLight.AddChild("Specular", NodeType::Float);
+        pointLight.AddChild("Position", NodeType::Float);
+
+        Node spotLightArr("spotLights", NodeType::Array);
+        spotLightArr.AddChildN(spotLight, 2);
+
+        Node pointLightArr("pointLights", NodeType::Array);
+        pointLightArr.AddChildN(pointLight, 6);
+
+        desc.AddNode(dirLight);
+        desc.AddNode(pointLightArr);
+        desc.AddNode(spotLightArr);
+        
+        mPerSceneBuffer = std::make_unique<DynamicConstBuffer>(desc, deviceResources);
+    }
 }
