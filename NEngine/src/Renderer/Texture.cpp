8ab2330e6a888aa70f8c4cbd2bff902e91f676f4
Nikita Elsakov <vocasle@outlook.com>
2022-10-31 21:31:50 +0200

    Fix crash in Texture ctor

--- NEngine/src/Renderer/Texture.cpp
+++ NEngine/src/Renderer/Texture.cpp
@@ -1,5 +1,7 @@
 #include "NEngine/Renderer/Texture.h"
 
+#include <dxgiformat.h>
+
 namespace NEngine::Renderer {
 
 using namespace Microsoft::WRL;
@@ -25,34 +27,38 @@ Texture::Texture(Helpers::DeviceResources &deviceResources,
       mBindSlot(bindSlot)
 {
     const int arraySize = 1;
-    const int mipLevels = 0;
+    const int mipLevels = 1;
     const int cpuAccessFlags = 0;
     const int sampleCount = 1;
     const int sampleQuality = 0;
     const int miscFlags = 0;
 
-    CD3D11_TEXTURE2D_DESC desc(DXGI_FORMAT_R8G8B8A8_UNORM,
-                               image.GetWidth(),
-                               image.GetHeight(),
-                               arraySize,
-                               mipLevels,
-                               TextureBindTargetToBindFlag(mBindTarget),
-                               D3D11_USAGE_DEFAULT,
-                               cpuAccessFlags,
-                               sampleCount,
-                               sampleQuality,
-                               miscFlags);
+    const CD3D11_TEXTURE2D_DESC desc(DXGI_FORMAT_R8G8B8A8_UNORM_SRGB,
+                                     image.GetWidth(),
+                                     image.GetHeight(),
+                                     arraySize,
+                                     mipLevels,
+                                     TextureBindTargetToBindFlag(mBindTarget),
+                                     D3D11_USAGE_IMMUTABLE,
+                                     cpuAccessFlags,
+                                     sampleCount,
+                                     sampleQuality,
+                                     miscFlags);
 
     D3D11_SUBRESOURCE_DATA initData;
     initData.pSysMem = image.GetBytes();
     initData.SysMemPitch = image.GetChannels() * image.GetWidth();
 
     ComPtr<ID3D11Texture2D> texture;
-    HR(deviceResources.GetDevice()->CreateTexture2D(&desc, &initData, texture.GetAddressOf()))
+    HR(deviceResources.GetDevice()->CreateTexture2D(
+        &desc, &initData, texture.GetAddressOf()))
+    assert(texture.Get() && "Failed to create Texture2D");
 
     if (mBindTarget == TextureBindTarget::ShaderResourceView) {
-        CD3D11_SHADER_RESOURCE_VIEW_DESC srvDesc(texture.Get(), D3D11_SRV_DIMENSION_TEXTURE2D, desc.Format);
-        HR(deviceResources.GetDevice()->CreateShaderResourceView(texture.Get(), &srvDesc, mSRV.ReleaseAndGetAddressOf()))
+        CD3D11_SHADER_RESOURCE_VIEW_DESC srvDesc(
+            texture.Get(), D3D11_SRV_DIMENSION_TEXTURE2D, desc.Format);
+        HR(deviceResources.GetDevice()->CreateShaderResourceView(
+            texture.Get(), &srvDesc, mSRV.ReleaseAndGetAddressOf()))
     }
     else {
         UTILS_FATAL_ERROR("Not implemented");
