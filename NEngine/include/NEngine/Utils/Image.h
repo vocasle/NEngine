b62b7f9e59e8ea2e5d35eb8d62399d30a0fa132d
Nikita Elsakov <vocasle@outlook.com>
2022-10-31 21:31:02 +0200

    Add new ctor to construct Image from raw bytes

--- NEngine/include/NEngine/Utils/Image.h
+++ NEngine/include/NEngine/Utils/Image.h
@@ -4,62 +4,84 @@
 #include "NEngine/Utils/Utils.h"
 #include "stb_image/stb_image.h"
 
-namespace NEngine
+namespace NEngine {
+namespace Utils {
+class Image
 {
-	namespace Utils
-	{
-		class Image {
-		public:
-			Image(const std::string& filepath)
-				: m_bytes{ nullptr }
-				, m_width{ 0 }
-				, m_height{ 0 }
-				, m_channels{ 0 }
-			{
-				m_bytes = stbi_load(filepath.c_str(), &m_width, &m_height,
-					&m_channels, DESIRED_CHANNELS);
+public:
+    Image(std::vector<unsigned char> data,
+          int width,
+          int height,
+          int channels,
+          int bits)
+        : m_bytes(std::move(data)),
+          m_width(width),
+          m_height(height),
+          m_channels(channels)
+    {
+    }
+    explicit Image(const std::string &filepath)
+        : m_bytes{},
+          m_width{0},
+          m_height{0},
+          m_channels{0}
+    {
+        auto bytes = stbi_load(filepath.c_str(),
+                               &m_width,
+                               &m_height,
+                               &m_channels,
+                               DESIRED_CHANNELS);
 
-				if (!m_bytes) {
-					UtilsFatalError("Failed to load image from %s\n",
-						filepath.c_str());
-				}
-			}
+        if (!bytes) {
+            UtilsFatalError("Failed to load image from %s\n", filepath.c_str());
+            return;
+        }
 
-			~Image()
-			{
-				stbi_image_free(m_bytes);
-			}
+        const size_t sz = m_width * m_height * m_channels;
+        m_bytes.resize(sz);
+        memcpy(&m_bytes[0], bytes, sz);
+        stbi_image_free(bytes);
+    }
 
-			bool IsValid() const
-			{
-				return m_bytes != nullptr;
-			}
+    ~Image()
+    {
+    }
 
-			uint32_t GetWidth() const
-			{
-				return static_cast<uint32_t>(m_width);
-			}
-			uint32_t GetHeight() const
-			{
-				return static_cast<uint32_t>(m_height);
-			}
-			uint32_t GetChannels() const
-			{
-				return static_cast<uint32_t>(m_channels);
-			}
-			uint8_t* GetBytes() const
-			{
-				return m_bytes;
-			}
+    bool
+    IsValid() const
+    {
+        return m_bytes.size() > 0;
+    }
 
-			static const int DESIRED_CHANNELS = 4; // for RGBA
+    uint32_t
+    GetWidth() const
+    {
+        return static_cast<uint32_t>(m_width);
+    }
+    uint32_t
+    GetHeight() const
+    {
+        return static_cast<uint32_t>(m_height);
+    }
+    uint32_t
+    GetChannels() const
+    {
+        return static_cast<uint32_t>(m_channels);
+    }
+    const unsigned char *
+    GetBytes() const
+    {
+        return &m_bytes[0];
+    }
 
-		private:
-			uint8_t* m_bytes;
-			int m_width;
-			int m_height;
-			int m_channels;
-		};
+    static const int DESIRED_CHANNELS = 4;  // for RGBA
 
-	}
-}
+private:
+    std::vector<unsigned char> m_bytes;
+    int m_width;
+    int m_height;
+    int m_channels;
+};
+
+}  // namespace Utils
+}  // namespace NEngine
