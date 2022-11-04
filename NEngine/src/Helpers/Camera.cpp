287ed5ababfdb1831591b7edac836186e38db997
Nikita Elsakov <vocasle@outlook.com>
2022-11-04 17:28:55 +0200

    Reflect Z in camera
    This is due to the fact gLTF models use right handed coordinate system

--- NEngine/src/Helpers/Camera.cpp
+++ NEngine/src/Helpers/Camera.cpp
@@ -1,10 +1,13 @@
 #include "NEngine/Helpers/Camera.h"
-#include "NEngine/Input/Keyboard.h"
-#include "NEngine/Input/Mouse.h"
-#include "NEngine/Utils/Utils.h"
+
+#include <corecrt_math_defines.h>
 
 #include <cmath>
-#include <corecrt_math_defines.h>
+
+#include "NEngine/Input/Keyboard.h"
+#include "NEngine/Input/Mouse.h"
+#include "NEngine/Math/Math.h"
+#include "NEngine/Utils/Utils.h"
 
 namespace NEngine {
 namespace Helpers {
@@ -13,10 +16,12 @@ using namespace Math;
 using namespace Input;
 
 Camera::Camera()
-    : Camera({0.0f, 0.0f, 0.0f}) {
+    : Camera({0.0f, 0.0f, 0.0f})
+{
 }
 
-Camera::Camera(const Vec3D &cameraPos) {
+Camera::Camera(const Vec3D &cameraPos)
+{
     m_Pos = cameraPos;
     m_Pitch = 0.0f;
     m_Yaw = (float)M_PI_2;
@@ -27,13 +32,18 @@ Camera::Camera(const Vec3D &cameraPos) {
 }
 
 Mat4X4
-Camera::GetViewMat() const {
+Camera::GetViewMat() const
+{
     const Vec3D direction = MathVec3DAddition(&m_Pos, &m_At);
-    return MathMat4X4ViewAt(&m_Pos, &direction, &m_Up);
+    auto reflectZ = MathMat4X4Identity();
+    reflectZ.A22 = -1;
+
+    return reflectZ * MathMat4X4ViewAt(&m_Pos, &direction, &m_Up);
 }
 
 Mat4X4
-Camera::GetProjMat() const {
+Camera::GetProjMat() const
+{
     return MathMat4X4PerspectiveFov(MathToRadians(m_fov),
                                     m_backBufferWidth / m_backBufferHeight,
                                     m_zNear,
@@ -41,10 +51,12 @@ Camera::GetProjMat() const {
 }
 
 void
-Camera::UpdateSpeed() {
+Camera::UpdateSpeed()
+{
     if (Keyboard::Get().IsKeyDown(VK_OEM_PLUS)) {
         m_Speed += 0.5f;
-    } else if (Keyboard::Get().IsKeyDown(VK_OEM_MINUS)) {
+    }
+    else if (Keyboard::Get().IsKeyDown(VK_OEM_MINUS)) {
         m_Speed -= 0.5f;
     }
 
@@ -52,40 +64,46 @@ Camera::UpdateSpeed() {
 }
 
 void
-Camera::ProcessKeyboard(double deltaMillis) {
+Camera::ProcessKeyboard(double deltaMillis)
+{
     UpdateSpeed();
     Vec3D cameraFocus = m_At;
     const float delta = (float)deltaMillis * CAMERA_SENSITIVITY * m_Speed;
 
-    if (Keyboard::Get().IsKeyDown(VK_LEFT) ||
-        Keyboard::Get().IsKeyDown('A')) {
+    if (Keyboard::Get().IsKeyDown(VK_LEFT) || Keyboard::Get().IsKeyDown('A')) {
         Vec3D right = MathVec3DCross(&cameraFocus, &m_Up);
         MathVec3DNormalize(&right);
         right = MathVec3DModulateByScalar(&right, delta);
         m_Pos = MathVec3DAddition(&m_Pos, &right);
-    } else if (Keyboard::Get().IsKeyDown(VK_RIGHT) ||
-               Keyboard::Get().IsKeyDown('D')) {
+    }
+    else if (Keyboard::Get().IsKeyDown(VK_RIGHT) ||
+             Keyboard::Get().IsKeyDown('D')) {
         Vec3D right = MathVec3DCross(&cameraFocus, &m_Up);
         MathVec3DNormalize(&right);
         right = MathVec3DModulateByScalar(&right, delta);
         m_Pos = MathVec3DSubtraction(&m_Pos, &right);
-    } else if (Keyboard::Get().IsKeyDown(VK_UP)) {
+    }
+    else if (Keyboard::Get().IsKeyDown(VK_UP)) {
         const Vec3D up = MathVec3DModulateByScalar(&m_Up, delta);
         m_Pos = MathVec3DAddition(&m_Pos, &up);
-    } else if (Keyboard::Get().IsKeyDown(VK_DOWN)) {
+    }
+    else if (Keyboard::Get().IsKeyDown(VK_DOWN)) {
         const Vec3D up = MathVec3DModulateByScalar(&m_Up, delta);
         m_Pos = MathVec3DSubtraction(&m_Pos, &up);
-    } else if (Keyboard::Get().IsKeyDown('W')) {
+    }
+    else if (Keyboard::Get().IsKeyDown('W')) {
         cameraFocus = MathVec3DModulateByScalar(&cameraFocus, delta);
         m_Pos = MathVec3DAddition(&m_Pos, &cameraFocus);
-    } else if (Keyboard::Get().IsKeyDown('S')) {
+    }
+    else if (Keyboard::Get().IsKeyDown('S')) {
         cameraFocus = MathVec3DModulateByScalar(&cameraFocus, delta);
         m_Pos = MathVec3DSubtraction(&m_Pos, &cameraFocus);
     }
 }
 
 void
-Camera::UpdateVectors() {
+Camera::UpdateVectors()
+{
     const float d = cosf(m_Pitch);
     const float x = d * cosf(m_Yaw);
     const float z = d * sinf(m_Yaw);
@@ -104,7 +122,8 @@ Camera::UpdateVectors() {
 }
 
 void
-Camera::ProcessMouse(double deltaMillis) {
+Camera::ProcessMouse(double deltaMillis)
+{
     const Vec2D mouseDelta = Mouse::Get().GetMouseDelta();
 
     static const float MAX_PITCH = (float)(M_PI_2 - 0.1);
@@ -122,9 +141,9 @@ Camera::ProcessMouse(double deltaMillis) {
     //	return;
     // }
 
-    // const float xOffset = (Mouse->MousePos.X - xLastPos) * MOUSE_SENSITIVITY *
-    // (float)deltaMillis; const float yOffset = (yLastPos - Mouse->MousePos.Y) *
-    // MOUSE_SENSITIVITY * (float)deltaMillis;
+    // const float xOffset = (Mouse->MousePos.X - xLastPos) * MOUSE_SENSITIVITY
+    // * (float)deltaMillis; const float yOffset = (yLastPos -
+    // Mouse->MousePos.Y) * MOUSE_SENSITIVITY * (float)deltaMillis;
 
     // xLastPos = Mouse->MousePos.X;
     // yLastPos = Mouse->MousePos.Y;
@@ -139,32 +158,37 @@ Camera::ProcessMouse(double deltaMillis) {
 }
 
 void
-Camera::SetViewDimensions(uint32_t width, uint32_t height) {
+Camera::SetViewDimensions(uint32_t width, uint32_t height)
+{
     m_backBufferWidth = width;
     m_backBufferHeight = height;
 }
 
 void
-Camera::SetZNear(const float zNear) {
+Camera::SetZNear(const float zNear)
+{
     m_zNear = zNear;
 }
 
 void
-Camera::SetZFar(const float zFar) {
+Camera::SetZFar(const float zFar)
+{
     m_zFar = zFar;
 }
 
 void
-Camera::LookAt(const Vec3D &pos, const Vec3D &target, const Vec3D &up) {
+Camera::LookAt(const Vec3D &pos, const Vec3D &target, const Vec3D &up)
+{
     m_Pos = pos;
     m_At = target;
     m_Up = up;
 }
 
 void
-Camera::SetFov(float fov) {
+Camera::SetFov(float fov)
+{
     m_fov = fov;
 }
 
-}
-}
+}  // namespace Helpers
+}  // namespace NEngine
