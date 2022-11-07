733e6b06815a2346eb32c5ec6aa9103a32fd9f04
Nikita Elsakov <vocasle@outlook.com>
2022-11-07 14:39:05 +0200

    Update to match BasePass.hlsli definitions

--- NEngine/include/NEngine/Helpers/LightHelper.h
+++ NEngine/include/NEngine/Helpers/LightHelper.h
@@ -7,115 +7,69 @@ namespace Helpers {
 
 using namespace Math;
 
-struct Color {
-    Color()
-        : R{0},
-          G{0},
-          B{0},
-          A{0} {
-    }
-
-    Color(float r, float g, float b, float a)
-        : R{r},
-          G{g},
-          B{b},
-          A{a} {
-    }
-
-    float R;
-    float G;
-    float B;
-    float A;
-};
-
-inline Color
-ColorFromRGBA(const float r,
-              const float g,
-              const float b,
-              const float a) {
-    return {r, g, b, a};
-}
-
-struct DirectionalLight {
+struct DirectionalLight
+{
     DirectionalLight()
         : Ambient{},
           Diffuse{},
           Specular{},
-          Position{},
-          Radius{0} {
+          Position{}
+    {
     }
 
-    Color Ambient;
-    Color Diffuse;
-    Color Specular;
-    Vec3D Position;
-    float Radius;
+    Vec4D Ambient;
+    Vec4D Diffuse;
+    Vec4D Specular;
+    Vec4D Position;  // W is radius
 };
 
-struct PointLight {
+struct PointLight
+{
     PointLight()
         : Ambient{},
           Diffuse{},
           Specular{},
-          Position{},
-          Range{0},
-          Att{},
-          pad{0} {
+          Position{}
+    {
     }
 
-    Color Ambient;
-    Color Diffuse;
-    Color Specular;
-    Vec3D Position;
-    float Range;
-    Vec3D Att;
-    float pad;
+    Vec4D Ambient;
+    Vec4D Diffuse;
+    Vec4D Specular;
+    Vec4D Position;  // W - is range
 };
 
-struct SpotLight {
+struct SpotLight
+{
     SpotLight()
         : Ambient{},
           Diffuse{},
           Specular{},
           Position{},
-          Range{0},
-          Direction{},
-          Spot{0},
-          Att{},
-          pad{0} {
+          Direction{}
+    {
     }
 
-    Color Ambient;
-    Color Diffuse;
-    Color Specular;
-    Vec3D Position;
-    float Range;
-    Vec3D Direction;
-    float Spot;
-    Vec3D Att;
-    float pad;
+    Vec4D Ambient;
+    Vec4D Diffuse;
+    Vec4D Specular;
+    Vec4D Position;   // W - is range
+    Vec4D Direction;  // W - is spot
 };
 
-struct Material {
+struct Material
+{
     Material()
-        : Ambient{},
-          Diffuse{},
-          Specular{},
-          Reflection{} {
+        : BaseColor(),
+          Metalness(0),
+          Roughness(0)
+    {
     }
 
-    Material(Color ambient, Color diffuse, Color specular, Color reflection)
-        : Ambient(ambient),
-          Diffuse(diffuse),
-          Specular(specular),
-          Reflection(reflection) {
-    }
-
-    Color Ambient;
-    Color Diffuse;
-    Color Specular;
-    Color Reflection;
+    Vec4D BaseColor;
+    float Metalness;
+    float Roughness;
 };
 
-}
-}
+}  // namespace Helpers
+}  // namespace NEngine
