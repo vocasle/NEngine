bfcbee9bdec56bc3b5b5976c22fe4c08f29c129b
Nikita Elsakov <vocasle@outlook.com>
2022-12-15 14:50:24 +0200

    Move all init login into Engine class

--- NEngine/include/NEngine/Engine.h
+++ NEngine/include/NEngine/Engine.h
@@ -1,9 +1,28 @@
 #pragma once
 
+#include <Windows.h>
+
+#include "NEngine/Helpers/DeviceResources.h"
+
 namespace NEngine {
+
 class Engine
 {
 public:
     Engine(int argc, const char *argv[]);
+
+    auto OnWindowSizeChanged(long width, long height) -> void;
+    auto Update() -> void;
+
+private:
+    auto CreateDefaultWindow() -> void;
+    auto EngineLoop() -> void;
+    auto InitImGui() -> void;
+    auto InitDeviceResources() -> void;
+
+    constexpr static auto ENGINE_WINDOW_CLASS = L"NEngineWindowClass";
+
+    HWND mWnd = nullptr;
+    NEngine::Helpers::DeviceResources mDeviceResources;
 };
 }  // namespace NEngine
