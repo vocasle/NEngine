68e4d5df3363a4d5fec5b19c68631a82fc9bb14c
Nikita Elsakov <vocasle@gmail.com>
2023-06-05 13:30:32 +0300

    Use BufferCreateInfo to construct buffer

--- NEngine/include/buffer.h
+++ NEngine/include/buffer.h
@@ -9,122 +9,118 @@
 
 namespace NEngine {
 
+namespace BufferUtils {
+void CreateBuffer(VkDevice              device,
+                  VkPhysicalDevice      physicalDevice,
+                  VkDeviceSize          size,
+                  VkBufferUsageFlags    usage,
+                  VkMemoryPropertyFlags properties,
+                  VkBuffer             &buffer,
+                  VkDeviceMemory       &buffer_memory);
+
+void CopyBuffer(VkDevice      device,
+                VkCommandPool transferCmdPool,
+                VkQueue       transferQueue,
+                VkBuffer      src_buffer,
+                VkBuffer      dst_buffer,
+                VkDeviceSize  size);
+}  // namespace BufferUtils
+
+template <typename T>
+struct BufferCreateInfo
+{
+    VkDevice           device;
+    VkPhysicalDevice   physicalDevice;
+    VkCommandPool      transferCmdPool;
+    VkQueue            transferQueue;
+    std::vector<T>     data;
+    VkBufferUsageFlags usageFlags;
+};
+
 template <typename Data>
 class Buffer
 {
 public:
-    Buffer(VkDevice          device,
-           VkPhysicalDevice  physicalDevice,
-           std::vector<Data> data)
-        : m_data(std::move(data))
+    Buffer(const BufferCreateInfo<Data> &createInfo)
+        : m_data(createInfo.data),
+          m_buffer(nullptr),
+          m_bufferMemory(nullptr),
+          m_device(createInfo.device)
     {
-        CreateBuffer(device, physicalDevice);
-    }
-
-    static void
-    CreateBuffer(VkDevice              device,
-                 VkPhysicalDevice      physicalDevice,
-                 VkDeviceSize          size,
-                 VkBufferUsageFlags    usage,
-                 VkMemoryPropertyFlags properties,
-                 VkBuffer             &buffer,
-                 VkDeviceMemory       &buffer_memory)
-    {
-        const VulkanUtils::QueueFamilyIndices indices =
-            VulkanUtils::FindQueueFamilies(physicalDevice, nullptr);
-        const std::set<uint32_t> unique_queue_indices = {
-            indices.transfer_family.value(), indices.graphics_family.value()};
-        std::vector<uint32_t> queue_indices(unique_queue_indices.size());
-        std::copy(unique_queue_indices.begin(),
-                  unique_queue_indices.end(),
-                  queue_indices.begin());
-
-        VkBufferCreateInfo info{};
-        info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-        info.size = size;
-        info.usage = usage;
-        info.sharingMode = indices.GetSharingMode();
-        info.queueFamilyIndexCount = std::size(queue_indices);
-        info.pQueueFamilyIndices = queue_indices.data();
-
-        VKRESULT(vkCreateBuffer(device, &info, nullptr, &buffer));
-
-        VkMemoryRequirements memory_requirements;
-        vkGetBufferMemoryRequirements(device, buffer, &memory_requirements);
-
-        VkMemoryAllocateInfo alloc_info{};
-        alloc_info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-        alloc_info.allocationSize = memory_requirements.size;
-        alloc_info.memoryTypeIndex = find_memory_type(
-            physicalDevice, memory_requirements.memoryTypeBits, properties);
-
-        VKRESULT(
-            vkAllocateMemory(device, &alloc_info, nullptr, &buffer_memory));
-
-        VKRESULT(vkBindBufferMemory(device, buffer, buffer_memory, 0));
+        CreateBuffer(createInfo);
     }
 
     void
-    CopyBuffer(VkDevice      device,
-               VkCommandPool transferCmdPool,
-               VkQueue       transferQueue,
-               VkBuffer      src_buffer,
-               VkBuffer      dst_buffer,
-               VkDeviceSize  size)
+    Destroy()
     {
-        const VkCommandBuffer cb =
-            VulkanUtils::BeginSingleTimeCommands(device, transferCmdPool);
+        vkDestroyBuffer(m_device, m_buffer, nullptr);
+        vkFreeMemory(m_device, m_bufferMemory, nullptr);
+        m_buffer = nullptr;
+        m_bufferMemory = nullptr;
+    }
 
-        VkBufferCopy copy_region{};
-        copy_region.size = size;
-        vkCmdCopyBuffer(cb, src_buffer, dst_buffer, 1, &copy_region);
+    [[nodiscard]] VkBuffer
+    Get() const
+    {
+        return m_buffer;
+    }
 
-        VulkanUtils::EndSingleTimeCommands(
-            cb, device, transferCmdPool, transferQueue);
+    ~Buffer()
+    {
+        ASSERT(m_buffer == nullptr, "ERROR: Destroy() was not called!");
+        ASSERT(m_bufferMemory == nullptr, "ERROR: Destroy() was not called!");
     }
 
 private:
     void
-    CreateBuffer(VkDevice device, VkPhysicalDevice physicalDevice)
+    CreateBuffer(const BufferCreateInfo<Data> &createInfo)
     {
         const VkDeviceSize buffer_size = sizeof(m_data[0]) * m_data.size();
 
         VkBuffer       staging_buffer;
         VkDeviceMemory staging_buffer_memory;
-        CreateBuffer(device,
-                     physicalDevice,
-                     buffer_size,
-                     VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-                     VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
-                         VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-                     staging_buffer,
-                     staging_buffer_memory);
+        BufferUtils::CreateBuffer(createInfo.device,
+                                  createInfo.physicalDevice,
+                                  buffer_size,
+                                  VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                                  VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+                                      VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                                  staging_buffer,
+                                  staging_buffer_memory);
 
         void *data = nullptr;
-        VKRESULT(vkMapMemory(
-            device, staging_buffer_memory, 0, buffer_size, 0, &data));
+        VKRESULT(vkMapMemory(createInfo.device,
+                             staging_buffer_memory,
+                             0,
+                             buffer_size,
+                             0,
+                             &data));
 
         memcpy(data, m_data.data(), buffer_size);
-        vkUnmapMemory(device, staging_buffer_memory);
+        vkUnmapMemory(createInfo.device, staging_buffer_memory);
 
-        CreateBuffer(device,
-                     physicalDevice,
-                     buffer_size,
-                     VK_BUFFER_USAGE_TRANSFER_DST_BIT |
-                         VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
-                     VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-                     m_buffer,
-                     m_bufferMemory);
+        BufferUtils::CreateBuffer(createInfo.device,
+                                  createInfo.physicalDevice,
+                                  buffer_size,
+                                  createInfo.usageFlags,
+                                  VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                                  m_buffer,
+                                  m_bufferMemory);
 
-        CopyBuffer(staging_buffer, m_buffer, buffer_size);
+        BufferUtils::CopyBuffer(createInfo.device,
+                                createInfo.transferCmdPool,
+                                createInfo.transferQueue,
+                                staging_buffer,
+                                m_buffer,
+                                buffer_size);
 
-        vkDestroyBuffer(device, staging_buffer, nullptr);
-        vkFreeMemory(device, staging_buffer_memory, nullptr);
+        vkDestroyBuffer(createInfo.device, staging_buffer, nullptr);
+        vkFreeMemory(createInfo.device, staging_buffer_memory, nullptr);
     }
 
     std::vector<Data> m_data;
-
-    VkBuffer       m_buffer;
-    VkDeviceMemory m_bufferMemory;
+    VkBuffer          m_buffer;
+    VkDeviceMemory    m_bufferMemory;
+    VkDevice          m_device;
 };
 }  // namespace NEngine
--- NEngine/src/buffer.cpp
+++ NEngine/src/buffer.cpp
@@ -1,1 +1,64 @@
 #include "buffer.h"
+
+namespace NEngine::BufferUtils {
+void
+CreateBuffer(VkDevice              device,
+             VkPhysicalDevice      physicalDevice,
+             VkDeviceSize          size,
+             VkBufferUsageFlags    usage,
+             VkMemoryPropertyFlags properties,
+             VkBuffer             &buffer,
+             VkDeviceMemory       &buffer_memory)
+{
+    const VulkanUtils::QueueFamilyIndices indices =
+        VulkanUtils::FindQueueFamilies(physicalDevice, nullptr);
+    const std::set<uint32_t> unique_queue_indices = {
+        indices.transfer_family.value(), indices.graphics_family.value()};
+    std::vector<uint32_t> queue_indices(unique_queue_indices.size());
+    std::copy(unique_queue_indices.begin(),
+              unique_queue_indices.end(),
+              queue_indices.begin());
+
+    VkBufferCreateInfo info{};
+    info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    info.size = size;
+    info.usage = usage;
+    info.sharingMode = indices.GetSharingMode();
+    info.queueFamilyIndexCount = std::size(queue_indices);
+    info.pQueueFamilyIndices = queue_indices.data();
+
+    VKRESULT(vkCreateBuffer(device, &info, nullptr, &buffer));
+
+    VkMemoryRequirements memory_requirements;
+    vkGetBufferMemoryRequirements(device, buffer, &memory_requirements);
+
+    VkMemoryAllocateInfo alloc_info{};
+    alloc_info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    alloc_info.allocationSize = memory_requirements.size;
+    alloc_info.memoryTypeIndex = find_memory_type(
+        physicalDevice, memory_requirements.memoryTypeBits, properties);
+
+    VKRESULT(vkAllocateMemory(device, &alloc_info, nullptr, &buffer_memory));
+
+    VKRESULT(vkBindBufferMemory(device, buffer, buffer_memory, 0));
+}
+
+void
+CopyBuffer(VkDevice      device,
+           VkCommandPool transferCmdPool,
+           VkQueue       transferQueue,
+           VkBuffer      src_buffer,
+           VkBuffer      dst_buffer,
+           VkDeviceSize  size)
+{
+    const VkCommandBuffer cb =
+        VulkanUtils::BeginSingleTimeCommands(device, transferCmdPool);
+
+    VkBufferCopy copy_region{};
+    copy_region.size = size;
+    vkCmdCopyBuffer(cb, src_buffer, dst_buffer, 1, &copy_region);
+
+    VulkanUtils::EndSingleTimeCommands(
+        cb, device, transferCmdPool, transferQueue);
+}
+}  // namespace NEngine::BufferUtils
